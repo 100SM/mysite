f4f2d9b260c15dc6bbd2491ced96bd460d61bbec
BIT <BIT@DESKTOP-MTA1CLE>
2022-01-18 11:34:46 +0900

    add listener

--- mysite02/src/main/java/com/poscoict/mysite/controller/MainController.java
+++ mysite02/src/main/java/com/poscoict/mysite/controller/MainController.java
@@ -16,7 +16,7 @@ public class MainController extends HttpServlet {
 
 	public void init() throws ServletException {
 		String configPath = getServletConfig().getInitParameter("config");
-		System.out.println("MainController.init() called" + configPath);
+		System.out.println("MainController.init() called: " + configPath);
 		
 		super.init();
 	}
