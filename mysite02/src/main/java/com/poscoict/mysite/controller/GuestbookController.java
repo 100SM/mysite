bfe8aaf35500b93b2a49e23f3f1fb51f7ff5ed96
BIT <BIT@DESKTOP-MTA1CLE>
2022-01-18 10:43:39 +0900

    mysite02 - add Encoding filter

--- mysite02/src/main/java/com/poscoict/mysite/controller/GuestbookController.java
+++ mysite02/src/main/java/com/poscoict/mysite/controller/GuestbookController.java
@@ -16,8 +16,6 @@ public class GuestbookController extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");
-
 		String actionName = request.getParameter("a");
 
 		ActionFactory af = new GuestbookActionFactory();
--- mysite02/src/main/java/com/poscoict/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/poscoict/mysite/controller/UserController.java
@@ -16,8 +16,6 @@ public class UserController extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");
-
 		String actionName = request.getParameter("a");
 
 		ActionFactory af = new UserActionFcatory();
