1ad862c0eb000749bd66cdc74516cc9e8873ac69
BIT <BIT@DESKTOP-MTA1CLE>
2022-01-26 12:37:29 +0900

    mysite03 - logback(sl4j) 적용

--- mysite03/src/main/java/com/poscoict/mysite/exception/GlobalExceptionHandler.java
+++ mysite03/src/main/java/com/poscoict/mysite/exception/GlobalExceptionHandler.java
@@ -3,18 +3,25 @@ package com.poscoict.mysite.exception;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
+	private static final Log LOGGER = LogFactory.getLog(GlobalExceptionHandler.class);
+	
+	
 	@ExceptionHandler(Exception.class)
 	public String ExceptionHandler(Model model, Exception e) {
 		// 1. 로깅
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
+		LOGGER.error(errors.toString());
+		
 		System.out.println(errors.toString());
 
 		model.addAttribute("exception", errors.toString());
